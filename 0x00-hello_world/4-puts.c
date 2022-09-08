ca20d23d4a51b83f736e8966e01b8d34d599190c
Chawana Maseka <chawana.maseka@gmail.com>
2022-09-08 16:39:20 -0700

    Task 4 Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,13 @@
  *
  *  * main - Entry point
  *
- *   *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
  *
- *    * Return: Always 0 (Success)
+ *    * Return: 0
  *
  *     */
+
+
 
 int main(void)
 
