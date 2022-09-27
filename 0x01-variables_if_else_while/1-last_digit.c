e55d89bcb0ff11f5e1cb12a4b1c0f00e4e721781
Chawana Maseka <chawana.maseka@gmail.com>
2022-09-27 12:46:57 -0700

    The line of life is a ragged diagonal between duty and desire

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,65 +1,31 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
-
-/**
- *
- * * main - print if the number is postive, zero, or negative
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is positive, zero, or negative
- *
- * * Return: 0
- *
- * */
-
+#include <stdlib.h>
+#include <time.h>
+/*
+ * main - Entry point
+ * Return: Always 0 if successful)
+ */
 int main(void)
-
 {
-
 	int n;
 
-	int l;
-
-
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-
-	l = n % 10;
-
-
-
-	if (l > 5)
-
+	/*
+	 * your code goes there
+	 */
+	if (n % 10 > 5)
 	{
-
-			printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-
-	else if (l == 0)
-
+	else if (n % 10 == 0)
 	{
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-
 	else
-
 	{
-
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-
 	return (0);
-
 }
+
