4132d35d5ed0c5ee5790ff8ba6dedba740459a9a
Chawana Maseka <chawana.maseka@gmail.com>
2022-12-12 20:55:58 -0800

    C Rocks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the progrma prints the alphabet in lower case'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
