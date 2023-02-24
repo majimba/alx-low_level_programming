3dcb48b6f4d06048bbc37db20aa3820b967e59b5
Chawana Maseka <chawana.maseka@gmail.com>
2023-02-24 02:17:47 -0800

    c is fun

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - natural numbers from n to 98.
+ * @n: input.
+ *
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
